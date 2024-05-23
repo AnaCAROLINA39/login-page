852b9457dfa5407644818115475be31491b6d385
AnaCAROLINA39 <anacarolina93silva@gmail.com>
2024-05-23 14:11:38 -0300

    inicial

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { DefaultLoginLayoutComponent } from "../../components/default-login-layout/default-login-layout.component";
+
+
+@Component({
+    selector: 'app-login',
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.css',
+    imports: [DefaultLoginLayoutComponent]
+})
+export class LoginComponent {
+
+}
