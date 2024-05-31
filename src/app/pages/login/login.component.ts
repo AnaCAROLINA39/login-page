3399239a8eabfc526a0e2c56da552f7ba93dc96c
AnaCAROLINA39 <anacarolina93silva@gmail.com>
2024-05-31 16:58:42 -0300

    valid o form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,6 +4,7 @@ import { DefaultLoginLayoutComponent } from "../../components/default-login-layo
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -18,7 +19,7 @@ export class LoginComponent {
   loginForm!:FormGroup;
 
 
-  constructor(private router: Router, private LoginService:LoginService ){
+  constructor(private router: Router, private LoginService:LoginService ,private toastr: ToastrService){
     this.loginForm = new FormGroup ({
       email:new FormControl('',[Validators.required, Validators.email]),
       password:new FormControl('',[Validators.required, Validators.minLength(6)])
@@ -26,8 +27,8 @@ export class LoginComponent {
   }
   submit(){
     this.LoginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
-      next:() => console.log("sucessada"),
-    error:()=> console.log("error")})
+      next:() => this.toastr.success("Login feito com sucesso!"),
+    error:()=> this.toastr.error("Erro inesperado! Tente mas tarde")})
   }
   navigate(){
     this.router.navigate(["signup"])
