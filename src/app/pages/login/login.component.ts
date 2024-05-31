fe68724e2b93203ab4e73bdca3476e2de887973a
AnaCAROLINA39 <anacarolina93silva@gmail.com>
2024-05-31 16:13:01 -0300

    mudancas toastr

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,9 @@
+import { LoginService } from '../../components/services/login.service';
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from "../../components/default-login-layout/default-login-layout.component";
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,15 +11,26 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
     standalone: true,
     templateUrl: './login.component.html',
     styleUrl: './login.component.css',
-    imports: [DefaultLoginLayoutComponent,FormsModule,ReactiveFormsModule]
+    imports: [PrimaryInputComponent,DefaultLoginLayoutComponent,FormsModule,ReactiveFormsModule],
+    providers:[LoginService]
 })
 export class LoginComponent {
   loginForm!:FormGroup;
 
-  constructor(){
-    this.loginForm = new FormGroup({
-      email: new FormControl ('',[Validators.required, Validators.email]),
-      password: new FormControl ('',[Validators.required, Validators.minLength(6)])
+
+  constructor(private router: Router, private LoginService:LoginService ){
+    this.loginForm = new FormGroup ({
+      email:new FormControl('',[Validators.required, Validators.email]),
+      password:new FormControl('',[Validators.required, Validators.minLength(6)])
     })
   }
+  submit(){
+    this.LoginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next:() => console.log("sucessada"),
+    error:()=> console.log("error")})
   }
+  navigate(){
+    this.router.navigate(["signup"])
+  }
+  }
+
